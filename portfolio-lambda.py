5f06708e62a45fb4ec164c6bebc8ef6406af49bc
Tapan Joshi <tapan.mjo92@gmail.com>
2018-10-27 18:02:03 +1100

    added sns

--- portfolio-lambda.py
+++ portfolio-lambda.py
@@ -4,6 +4,9 @@ import StringIO
 import mimetypes
 
 def lambda_handler(event, context):
+    sns=boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:989447057278:deploy_PortfolioTopic')
+
     s3=boto3.resource('s3')
 
     portfolio_bucket = s3.Bucket('portfolio.tapanj.info')
@@ -18,5 +21,8 @@ def lambda_handler(event, context):
             portfolio_bucket.upload_fileobj(obj, nm,
               ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             portfolio_bucket.Object(nm).Acl().put(ACL = 'public-read')
+
+    print "Job Done!"
+    topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
 
     return 'Hello from Lambda'
